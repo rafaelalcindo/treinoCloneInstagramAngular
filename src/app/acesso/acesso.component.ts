8b6ef6235612fa653fb63dbbf0817fcc4a9abf43
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-02-07 16:16:46 -0200

    novas mudanças

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px, 0)' }),
+        animate('500ms 0s ease-in-out') // duração, delay e aceleração
+      ])
+    ]),
+
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0)' }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
+
+  public cadastro: boolean = false
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public exibirPanel(event: string): void {
+    this.cadastro = event === 'cadastro' ? true : false
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 import { AppComponent } from './app.component';
@@ -18,7 +20,8 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
