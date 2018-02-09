344df7ba21a7e8b7d85fdbc30ef98eaa15836f44
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-02-09 14:29:38 -0200

    modis

--- src/app/autenticacao-guard.service.ts
+++ src/app/autenticacao-guard.service.ts
@@ -1,7 +1,15 @@
+import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import { Autenticacao } from './autenticacao.service';
 
+@Injectable()
 export class AutenticacaoGuard implements CanActivate {
+
+    constructor(
+      private autenticacao: Autenticacao
+    ) {}
+
     canActivate(): boolean {
-        return true
+        return this.autenticacao.autenticado();
     }
 }
